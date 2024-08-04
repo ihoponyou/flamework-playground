258592aa23ac97efdfc8d472316d21430e9cdc32
noahmagcalas <noah.magcalas@gmail.com>
2024-08-04 12:18:54 -0500

    refactor: delete unused files

--- src/shared/key-from-value.ts
+++
@@ -1,5 +0,0 @@
-import Object from "@rbxts/object-utils";
-
-export function keyFromValue<T extends object>(object: T, value: T[keyof T]): keyof T | undefined {
-	return Object.keys(object)[(Object.values(object) as defined[]).indexOf(value as defined)] as keyof T | undefined;
-}
